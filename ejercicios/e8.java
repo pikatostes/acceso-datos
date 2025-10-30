b82ee64ed96e605bb362abaf97467b2061e16a03
Alejandro Rios <alejandro.rios.cyt@gmail.com>
2025-10-30 19:38:41 +0100

    Corrección E8

--- ejercicios/e8.java
+++ ejercicios/e8.java
@@ -3,9 +3,12 @@ import java.io.File;
 public class e8 {
     public static void main(String[] args) {
         File file = new File("notas.txt");
+        File renamedFile = new File("notas_OLD.txt");
 
         if (file.exists()) {
-            if (file.renameTo(new File("notas_OLD.txt"))) {
+            if (renamedFile.exists()) {
+                System.out.println("Ya hay un archivo con ese nombre.");
+            } else if (file.renameTo(renamedFile)) {
                 System.out.println("El archivo se ha renombrado correctamente.");
             } else {
                 System.out.println("Ya hay un archivo con ese nombre.");
